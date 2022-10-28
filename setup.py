71740414dfe7bc50c2f84d888e9660ed5d67e54a
jesseturland <jesse_Turland@hotmail.com>
2022-10-29 10:28:07 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from disutils.core import setup
 
 setup(
 
-	name = 'S3Uploader',
-	version='0.1demo',
-	packages=['s3uploader',],
+	name = 'braces',
+	packages=['braces',],
 	license='Creative Commons Attribution-Noncommercial-Share Alike license',
 	long_description=open('README.txt').read(),
 	url='https://github.com/jesseturland/braces'
