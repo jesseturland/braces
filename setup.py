8daad8858800a9a2f0c37b642f2787ac0a4770ae
jesseturland <jesse_Turland@hotmail.com>
2022-10-29 10:24:24 +1100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from disutils.core import setup
+
+setup(
+
+	name = 'S3Uploader',
+	version='0.1demo',
+	packages=['s3uploader',],
+	license='Creative Commons Attribution-Noncommercial-Share Alike license',
+	long_description=open('README.txt').read(),
+	url='https://github.com/jesseturland/braces'
+	)
